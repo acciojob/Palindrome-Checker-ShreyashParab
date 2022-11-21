50b5afbd74da4b7334a71cdccc8f2edb800a11cc
ShreyashParab <88625975+ShreyashParab@users.noreply.github.com>
2022-11-21 13:37:52 +0530

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,6 +1,21 @@
 // complete the given function
 
 function palindrome(str){
-
+  function isPalindrome(str){
+  var strRev=''
+for(let i = str.length-1 ; i>=0;i--)
+{
+ strRev += str[i]
 }
+
+ if(str.toUpperCase() == strRev.toUpperCase())
+   {
+      document.write('true')
+   }
+ else{
+      document.write('false')
+   }
+}
+  
+Palindrome('raceCAR')
 module.exports = palindrome
